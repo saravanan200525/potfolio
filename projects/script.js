a26fc43a9c3dfe0504026a0295232185cafadf6a
saravanan200525 <saravanan250105@gmail.com>
2024-08-10 12:55:40 +0530

    part5

--- projects/script.js
+++ projects/script.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Jigar Sable";
+            document.title = "Projects | Portfolio saravanan";
             $("#favicon").attr("href", "/images/favicon.png");
         }
         else {
